a79ae0efa43e5d295e0bfd5bba0d4c0b86ac0a1b
iammhardy <iamhardyusuf@gmail.com>
2026-03-27 09:08:47 +0100

    Remove stale ai controllers folder causing Zeitwerk error

--- app/controllers/ai/base_controller.rb
+++
@@ -1,9 +0,0 @@
-class Ai::BaseController < ApplicationController
-  before_action :require_ai_access!
-
-  private
-
-  def require_ai_access!
-    head :forbidden unless current_user.manager_or_above?
-  end
-end
